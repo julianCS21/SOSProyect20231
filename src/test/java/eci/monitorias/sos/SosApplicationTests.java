cd81f98c840469c08b81ad28f9c93fda7ee89057
rescate rescate <rescate@labinfo.is.escuelaing.edu.co>
2023-02-27 15:37:31 -0500

    inicio proyecto

---
+++ src/test/java/eci/monitorias/sos/SosApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.monitorias.sos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
