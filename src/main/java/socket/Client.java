f369f5a24710b6e5aeafe247c239a370a22b5822
rescate rescate <rescate@labinfo.is.escuelaing.edu.co>
2023-02-28 18:47:01 -0500

    socket cliente

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,13 +1,26 @@
 package socket;
 
 
+import eci.monitorias.sos.ViewUser;
 
+import java.net.Socket;
 
 public class Client {
 
 
+    private Socket client;
+    private ViewUser vista;
 
-
-
+    public Client(ViewUser vista) {
+        this.vista = vista;
+    }
 
 }
+
+
+
+
+
+
+
+
